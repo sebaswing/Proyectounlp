4b873ef6502debd767c52b51af6b2e129ff9f83c
Juan Sebastián Beltrán Leal <beltry1@hotmail.com>
2024-09-20 00:21:11 -0300

    agregando modularizado de la calculadora

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from src import calculator
 
 def main():
-    calculator.say_hello()
+    #calculator.say_hello()
+    print("welcome to the calculator!")
+    num1= float(input("escriba el primer numero"))
+    print("enter second number")
+    num2= float(input())
+    operator=input("digité el operador (+,-,*,/)")
+    result = calculator.calculate (num1,num2,operator)
+    print(f"the result is : {result}")
 
 if __name__== "__main__": #la idea acá es unicamente ejecutar el main cuando se llame directamente no en la consola interactiva
     main()
--- src/calculator.py
+++ src/calculator.py
@@ -1,2 +1,16 @@
-def say_hello():
-    print("Hola Mundo!")
+from src import operations
+
+#def say_hello():
+  #  print("Hola Mundo!")
+
+def calculate(a,b,operator):
+    if operator == "+":
+        return operations.add(a,b)
+    elif operator == "-":
+        return operations.substract(a,b)
+    elif operator == "*":
+        return operations.multiply(a,b)
+    if operator == "/":
+        return operations.divide(a,b)
+    else:
+        raise ValueError(f"Operador no valido{operator}")
